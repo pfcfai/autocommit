505148386d784e3108b8bf307cdacefa0d539e4d
pfcfai <pfcfia@gmail.com>
2021-03-24 08:53:49 +0000

    update linuxstamp

--- EXCEL_AutoUpdate/EXCEL_visualize_research.py
+++ EXCEL_AutoUpdate/EXCEL_visualize_research.py
@@ -4,32 +4,53 @@
 
 
 import pandas as pd
-
+'''
 df_nt=pd.read_csv('https://raw.githubusercontent.com/laurenttang/pfcfProject/main/Overseas3D/NTD.csv') 
 df_nt=pd.read_csv('https://raw.githubusercontent.com/laurenttang/pfcfProject/main/FTDOpenData015.csv')
 NTD=df_nt['NTD/USD'].iloc[-1]
 print(NTD)
-
+'''
 
 
 # making list of list 
 import openpyxl
 from openpyxl.reader.excel import load_workbook
+from datetime import timezone
+from datetime import datetime
 
-wb=openpyxl.load_workbook('/home/pfcf/pfcfProject/FTDOpenData015.xlsx')
+wb=openpyxl.load_workbook('/home/pfcf/pfcfProject/EXCEL_AutoUpdate/FTDOpenData015.xlsx')
 
 print(wb.sheetnames[0])
 sheet=wb['FTDOpenData015']
+rows=sheet.rows
 
-def recordlist(i):
-    date=sheet['A'+str(i)].value
-    twse=sheet['B'+str(i)].value
+def recordlist(row):
+    #date=sheet['A'+str(i)].value (original code)
+    
+    datestr=str(row[0].value)
+    try:
+        dt = datetime.strptime(datestr, "%Y%m%d")
+        #dt = datetime(2007, 1, 1)
+        datestr = dt.replace(tzinfo=timezone.utc).timestamp()
+    except:
+        pass
+    twse=row[1].value
     
 
-    record=[date,twse]
+    record=[datestr,twse]
     return record
 
-mylist=[recordlist(i) for i in range(3,3292)]
+#mylist=[recordlist(i) for i in range(3,3292)]
+mylist = []
+innerlist=[]
+'''
+for row in rows:
+    for cell in row:
+        innerlist.append(cell.value)
+    mylist.append(innerlist)
+'''
+mylist=[recordlist(row) for row in rows]
+del mylist[0]
 print(mylist)
 
 # save as json
