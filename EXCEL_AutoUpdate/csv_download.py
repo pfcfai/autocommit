f60f3311a36e28418e2a70d11bfc0dd9c591139d
spark <pfcfia@gmail.com>
2021-03-31 15:45:40 +0800

    add csv_download to track

---
+++ EXCEL_AutoUpdate/csv_download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+# In[1]:
+
+
+import requests
+csv_url='https://www.cbc.gov.tw/public/data/OpenData/外匯局/FTDOpenData015.csv'
+req = requests.get(csv_url)
+url_content = req.content
+csv_file = open('NTD.csv', 'wb')
+csv_file.write(url_content)
+csv_file.close()
+
+
+
