183d7e274886028a21a7801b130701f130db41b5
spark <pfcfia@gmail.com>
2021-05-10 16:49:07 +0800

    auto commit

--- EXCEL_AutoUpdate/payems/payems.py
+++ EXCEL_AutoUpdate/payems/payems.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
 
 # making list of list 
 import openpyxl
@@ -20,7 +22,7 @@ def recordlist(alpha,i):
     return record
 
 date=[recordlist('A',i) for i in range(8,sheet.max_row+1) if recordlist('A',i) != None]
-date=[each.strftime("%m/%y") for each in date]
+date=[each.strftime("%m/'%y") for each in date]
 print(date[-12:])
 unrate=[recordlist('E',i) for i in range(8,sheet.max_row+1) if recordlist('E',i) != None]
 print(unrate[-12:])
@@ -31,6 +33,9 @@ print(payems_icz[-12:])
 #print(len(sheet['G']))
 
 
+# In[47]:
+
+
 import json
 
 dict={}
@@ -39,7 +44,198 @@ dict['unrate']=unrate[-12:]
 dict['payems_icz']=payems_icz[-12:]
 print(dict)
 
-with open('/home/spark/autocommit/EXCEL_AutoUpdate/payems/payems.json', 'w') as json_file:
+with open('payems.json', 'w') as json_file:
     json.dump(dict, json_file)
 
+
+# In[4]:
+
+
+# making list of list 
+import openpyxl
+import statistics as stats
+from datetime import timezone
+from datetime import datetime
+
+book = openpyxl.load_workbook('經濟數據-FRED增益集.xlsx')
+
+#sheet = book.active
+sheet=book['薪資-月']
+
+def recordlist(alpha,i):
+    data=sheet[alpha+str(i)].value
+    record=data    # if you wanna only a list
+    #record=[data]    # if you wanna list of list
+    return record
+
+date=[recordlist('A',i) for i in range(8,sheet.max_row+1) if recordlist('A',i) != None]
+date=[each.strftime("%m/'%y") for each in date]
+print(date[-12:])
+hourpay_avg=[recordlist('B',i) for i in range(8,sheet.max_row+1) if recordlist('B',i) != None]
+print(hourpay_avg[-13:])
+
+hourpay_yoy=[round((hourpay_avg[j]/hourpay_avg[j-12]-1)*100,1) for j in range(len(hourpay_avg)) if j>12]
+print(hourpay_yoy[-12:])
+#print(sheet.max_row)
+#print(len(sheet['G']))
+
+
+# In[5]:
+
+
+import json
+
+dict={}
+dict['date']=date[-12:]
+dict['hourpay_avg']=hourpay_avg[-12:]
+dict['hourpay_yoy']=hourpay_yoy[-12:]
+print(dict)
+
+with open('hourpay.json', 'w') as json_file:
+    json.dump(dict, json_file)
+
+
+# In[10]:
+
+
+# making list of list 
+import openpyxl
+import statistics as stats
+from datetime import timezone
+from datetime import datetime
+
+book = openpyxl.load_workbook('經濟數據-FRED增益集.xlsx')
+
+#sheet = book.active
+sheet=book['就業-週']
+
+def recordlist(alpha,i):
+    data=sheet[alpha+str(i)].value
+    record=data    # if you wanna only a list
+    #record=[data]    # if you wanna list of list
+    return record
+
+date=[recordlist('A',i) for i in range(8,sheet.max_row+1) if recordlist('A',i) != None]
+date=[each.strftime("%m/%d") for each in date]
+print(date[-12:])
+initial_claim=[recordlist('B',i)/1000 for i in range(8,sheet.max_row+1) if recordlist('B',i) != None]
+print(initial_claim[-12:])
+continued_claim=[recordlist('E',i)/1000 for i in range(8,sheet.max_row+1) if recordlist('E',i) != None]
+print(continued_claim[-12:])
+#print(sheet.max_row)
+#print(len(sheet['G']))
+
+
+# In[11]:
+
+
+import json
+
+dict={}
+dict['date']=date[-12:]
+dict['initial_claim']=initial_claim[-12:]
+dict['continued_claim']=continued_claim[-12:]
+print(dict)
+
+with open('initial_claim.json', 'w') as json_file:
+    json.dump(dict, json_file)
+
+
+# In[16]:
+
+
+# making list of list 
+import openpyxl
+import statistics as stats
+from datetime import timezone
+from datetime import datetime
+
+book = openpyxl.load_workbook('經濟數據-FRED增益集.xlsx')
+
+#sheet = book.active
+def pairlist(alpha,beta,i):
+    data1=sheet[alpha+str(i)].value
+    data1=data1.replace(tzinfo=timezone.utc).timestamp()*1000
+    data2=sheet[beta+str(i)].value/1000
+    record=[data1,data2]    # if you wanna list of list
+    return record
+
+# PCE
+sheet=book['就業-週']
+initial_claim   = [pairlist('A','B',i) for i in range(8,sheet.max_row+1) if pairlist('A','B',i)[1] != '=NA()']
+continued_claim = [pairlist('D','E',i) for i in range(8,sheet.max_row+1-1) if pairlist('D','E',i)[1] != '=NA()']
+
+dict=initial_claim[-252:]
+dict2=continued_claim[-252:]
+
+import json
+
+with open('initial_claim.json', 'w') as json_file:
+    json.dump(dict, json_file)
+with open('continued_claim.json', 'w') as json_file:
+    json.dump(dict2, json_file)
+
+
+# 以下是開發時常用的小技巧
+# 包含 
+#  * datetime 轉換 字串或 linuxTimestamp
+#  * list 時序減法
+#  * Pandas read csv
+#  * openpyxl read excel whole sheet 寫法
+
+# In[37]:
+
+
+# datetime 轉換 字串或 linuxTimestamp
+from datetime import timezone
+from datetime import datetime
+print("datetime to linuxTime:",date[-1].replace(tzinfo=timezone.utc).timestamp()*1000)
+#date_time = date[-1].strftime("%m/%d/%Y, %H:%M:%S")
+date_time = date[-1].strftime("%m/%y")
+print("datetime to str:",date_time)	
+
+
+# In[25]:
+
+
+# list 時序減法
+sr=[i for i in range(1,11)]
+print(sr)
+ar=[sr[j]-sr[j-1] for j in range(len(sr)) if j>0]
+print(ar)
+
+
+# In[ ]:
+
+
+# Pandas read csv
+import pandas as pd
+
+df_nt=pd.read_csv('https://raw.githubusercontent.com/laurenttang/pfcfProject/main/Overseas3D/NTD.csv') 
+df_nt=pd.read_csv('https://raw.githubusercontent.com/laurenttang/pfcfProject/main/FTDOpenData015.csv')
+NTD=df_nt['NTD/USD'].iloc[-1]
+print(NTD)
+
+
+# In[ ]:
+
+
+# openpyxl read excel whole sheet 寫法
+import openpyxl
+import statistics as stats
+
+book = openpyxl.load_workbook('經濟數據-FRED增益集.xlsx')
+
+#sheet = book.active
+sheet=book['就業-月']
+
+rows = sheet.rows
+print(rows)
+values = []
+
+for row in rows:
+    for cell in row:
+        values.append(cell.value)
+print(sheet.max_row)
+print(len(sheet['G']))
 
