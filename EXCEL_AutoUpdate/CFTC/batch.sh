74c1a10b2ceb12e90f1178b765fc6f17c415e8b4
laurenttang <papasoup2008@gmail.com>
2023-09-25 18:31:01 +0800

    auto commit

--- EXCEL_AutoUpdate/CFTC/batch.sh
+++ EXCEL_AutoUpdate/CFTC/batch.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
+#filelist=$(ls /home/targets/autocommit/EXCEL_AutoUpdate/CFTC)
+
+#for name in $filelist;do
+#	if [[ "$name"==*.py ]]; then
+#		python /home/targets/autocommit/EXCEL_AutoUpdate/$name
+#	else
+#		echo $name
+#	fi
+#done
+
+#! /bin/bash
 filelist=$(ls /home/targets/autocommit/EXCEL_AutoUpdate/CFTC)
 
-for name in $filelist;do
-	if [[ "$name"==*.py ]]; then
-		python /home/targets/autocommit/EXCEL_AutoUpdate/$name
-	else
-		echo $name
-	fi
+for name in $filelist; do
+    if [[ "$name" == *.py ]]; then
+        python /home/targets/autocommit/EXCEL_AutoUpdate/$name
+    else
+        echo $name
+    fi
 done
